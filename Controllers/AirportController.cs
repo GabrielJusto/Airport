8fc0e98a14010e7eb9d15b2dc84252dd46c7f4ee
Gabriel Bonatto <gabriel.justo@acad.pucrs.br>
2025-07-01 20:35:17 -0300

    Add Airport controller.

---
+++ Controllers/AirportController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Airport.Controllers
+{
+    [ApiController]
+    [Route("airport")]
+    public class AirportController : ControllerBase
+    {
+        [HttpPost("create")]
+        public IActionResult Create([FromBody] object airportData)
+        {
+
+            return Ok(new { message = "Aeroporto criado com sucesso." });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,13 +3,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 
+app.MapControllers();
 app.UseHttpsRedirection();
 
 app.Run();
